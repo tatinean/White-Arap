45357377288d48c41dc5e380853fadc4ffe1412e
Sasha <tatinean@gmail.com>
2013-08-22 20:33:00 +0300

    Добавила jquery dropdown menu

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ $(document).ready(function () {
 	$('#container').masonry({
 		itemSelector: '.item',
 		  "gutter":0
-	})
+	});
+
+	$('#menu-toggle').on('click', function() {
+		$('.side-bar').slideToggle();
+	});
 });
