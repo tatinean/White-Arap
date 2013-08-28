e992e9108964a79eb223bb107a7884548e41e3f9
Sasha <tatinean@gmail.com>
2013-08-28 18:20:55 +0300

    Добавила выпадающее меню при маленьком разрешений.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
 	$('#container').masonry({
 		itemSelector: '.item',
 		  "gutter":0
-	});
+	})
+})
 
-	$('#menu-toggle').on('click', function() {
-		$('.side-bar').slideToggle();
-	});
-});
